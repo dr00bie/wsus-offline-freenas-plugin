01aa180ecb34513cd07d8f044abf287332f1d487
dr00bie <dr00bert@gmail.com>
2019-05-17 22:17:44 -0400

    Create post_install.sh

---
+++ post_install.sh
@@ -0,0 +1,3 @@
+# This script is run inside the jail after it is created and any packages installed. 
+# Enable services in /etc/rc.conf that need to start with the jail and apply any configuration customizations with 
+# this this script.
